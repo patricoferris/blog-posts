3662e3d43a0ee5324688bfa3f3cb4eb0842d05db
patricoferris <patrickferris17@gmail.com>
2018-08-31 22:07:16 +0100

    Add hover and tooltip effects

--- Exporting_Dependencies/index.js
+++ Exporting_Dependencies/index.js
@@ -4,6 +4,10 @@ var svg = d3.select("svg"),
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
     .force("charge", d3.forceManyBody())
@@ -17,7 +21,7 @@ d3.json("graph.json", function(error, graph) {
     .selectAll("line")
     .data(graph.links)
     .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .attr("stroke-width", function(d) { console.log(d); return Math.sqrt(d.value); });
 
   var node = svg.append("g")
     .attr("class", "nodes")
@@ -26,14 +30,23 @@ d3.json("graph.json", function(error, graph) {
     .enter().append("circle")
     .attr("r", 5)
     .attr("fill", function(d) { return color(d.group); })
-
+    .on("mouseover", mouseOver(.7)/*function(d) {
+      div.transition()
+          .duration(200)
+          .style("opacity", .9);
+      div.html(d.id)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+    }*/)
+    .on("mouseout", mouseOut/*function(d) {
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
+    }*/)
     .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
-
-  node.append("title")
-      .text(function(d) { return d.id; });
 
   simulation
       .nodes(graph.nodes)
@@ -57,6 +70,48 @@ d3.json("graph.json", function(error, graph) {
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   }
+
+  var linkedByIndex = {};
+    graph.links.forEach(function(d) {
+        linkedByIndex[d.source.index + "," + d.target.index] = 1;
+    });
+
+  function isConnected(a, b) {
+    return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
+  }
+
+  function mouseOver(opacity) {
+          return function(d) {
+              // check all other nodes to see if they're connected
+              // to this one. if so, keep the opacity at 1, otherwise
+              // fade
+              node.style("stroke-opacity", function(o) {
+                  thisOpacity = isConnected(d, o) ? 1 : opacity;
+                  return thisOpacity;
+              });
+              node.style("fill-opacity", function(o) {
+                  thisOpacity = isConnected(d, o) ? 1 : opacity;
+                  return thisOpacity;
+              });
+              // also style link accordingly
+              link.style("stroke-opacity", function(o) {
+                  return o.source === d || o.target === d ? 1 : opacity;
+              });
+              link.style("stroke", function(o){
+                  return o.source === d || o.target === d ? o.source.colour : "#ddd";
+              });
+          };
+      }
+
+      function mouseOut() {
+          node.style("stroke-opacity", 1);
+          node.style("fill-opacity", 1);
+          link.style("stroke-opacity", 1);
+          link.style("stroke", "#ddd");
+      }
+
+
+
 });
 
 function dragstarted(d) {
